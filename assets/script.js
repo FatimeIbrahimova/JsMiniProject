7e4e4be6e8dcc1e9bb1c16046630d6f8058e7b7a
FatimeIbrahimova <fatime.ibrahimova44@gmail.com>
2022-12-15 13:30:06 +0400

    task

--- assets/script.js
+++ assets/script.js
@@ -11,16 +11,16 @@ fetch('https://fakestoreapi.com/products/categories')
     )
 
 /* Navbar Scroll*/
-window.addEventListener("scroll", () => {
-    const nav = document.querySelector(".naw2");
-    const scrollValue = window.scrollY;
-    console.log(scrollValue);
-    if (scrollValue < 150) {
-        nav.classList.remove("nav-scroll");
-    } else {
-        nav.classList.add("nav-scroll");
-    }
-});
+// window.addEventListener("scroll", () => {
+//     const nav = document.querySelector(".naw2");
+//     const scrollValue = window.scrollY;
+//     console.log(scrollValue);
+//     if (scrollValue < 150) {
+//         nav.classList.remove("nav-scroll");
+//     } else {
+//         nav.classList.add("nav-scroll");
+//     }
+// });
 
 /*Back to Top */
 const upIcon = document.querySelector(".fa-arrow-up");
@@ -29,3 +29,50 @@ upIcon.addEventListener("click", () => {
     document.documentElement.scrollTop = 0;
 }
 )
+
+/*Desc and Comments*/
+const btnProductDesc=document.querySelector(".product-desc");
+const btnReview=document.querySelector(".review");
+const content=document.querySelector(".content");
+const comments=document.querySelector(".comments");
+btnProductDesc.addEventListener("click",(e)=>{
+e.preventDefault();
+    if(content.style.display="none"){
+        content.style.display="block";
+        comments.style.display="none";
+        btnProductDesc.classList.add("active");
+        btnReview.classList.remove("active");
+    } 
+})
+btnReview.addEventListener("click",(e)=>{
+    console.log("click");
+    e.preventDefault();
+    if(comments.style.display="none"){
+        comments.style.display="block";
+        btnReview.classList.add("active");
+        btnProductDesc.classList.remove("active");
+        content.style.display="none";
+    }
+})
+/*increase count */
+
+const iconUp=document.querySelector(".fa-chevron-up");
+const number=document.querySelector(".number");
+const iconDown=document.querySelector(".fa-chevron-down");
+let count=0;
+iconUp.addEventListener("click",()=>{
+    count++;
+    number.innerText=count;
+})
+iconDown.addEventListener("click",()=>{
+    if(number<=1){
+        count=0;
+        number.innerText=count; 
+    }
+    else{
+        count--;
+        number.innerText=count;
+    }
+})
+
+
